93542bbc0603ceb99eef7c4f8f9c608d76299e5d
Edwin <hgededwindaniel@gmail.com>
2024-03-25 12:09:37 -0600

    adding themes and colors

--- app/src/main/java/com/ehernndez/poketest/MainActivity.kt
+++ app/src/main/java/com/ehernndez/poketest/MainActivity.kt
@@ -1,18 +1,14 @@
 package com.ehernndez.poketest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    lateinit var txtHelloWorld: TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        txtHelloWorld = findViewById(R.id.txt_hello_world)
-        txtHelloWorld.text = "Hola mundo ahora en español"
-        
     }
 }
