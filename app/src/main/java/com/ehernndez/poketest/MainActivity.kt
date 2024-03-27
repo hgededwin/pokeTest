1edc547cd01cd8bdff5a2aaeb22b5c1089c46ea8
Edwin <hgededwindaniel@gmail.com>
2024-03-27 15:39:13 -0600

    adding changes to create new screen onboarding

--- app/src/main/java/com/ehernndez/poketest/MainActivity.kt
+++ app/src/main/java/com/ehernndez/poketest/MainActivity.kt
@@ -1,14 +1,46 @@
 package com.ehernndez.poketest
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.e("--->", "method onCreate was called")
+
+        /*
+           supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container_view, FirstFragment())
+            commit()
+        }
+         */
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.e("-->", "method onStart was called ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.e("--->", "method onStop was called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+
+        Log.e("--->", "method onRestart was called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.e("--->", "method onResume was called")
     }
 }
