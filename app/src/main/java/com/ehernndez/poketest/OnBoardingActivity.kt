3a79d1b9c17f78575de7443f19f25a644e84d123
Edwin <hgededwindaniel@gmail.com>
2024-04-03 10:19:33 -0600

    updating register activity

--- app/src/main/java/com/ehernndez/poketest/OnBoardingActivity.kt
+++ app/src/main/java/com/ehernndez/poketest/OnBoardingActivity.kt
@@ -1,15 +1,28 @@
 package com.ehernndez.poketest
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class OnBoardingActivity : AppCompatActivity() {
-
+    lateinit var btnStart: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_onboarding)
         Log.e("--->", "method onCreate was called")
+
+        btnStart = findViewById(R.id.btn_start)
+
+        btnStart.setOnClickListener {
+           val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        val email = "ehernndez@gmail.com"
+        Log.e("isValid email -->", email.isValidEmail().toString())
     }
 
     override fun onStart() {
@@ -24,6 +37,12 @@ class OnBoardingActivity : AppCompatActivity() {
         Log.e("--->", "method onStop was called")
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.e("--->", "method onDestroy was called")
+    }
+
     override fun onRestart() {
         super.onRestart()
 
@@ -36,3 +55,6 @@ class OnBoardingActivity : AppCompatActivity() {
         Log.e("--->", "method onResume was called")
     }
 }
+
+// extension that verify if an email is valid
+fun String.isValidEmail() = isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(this).matches()
