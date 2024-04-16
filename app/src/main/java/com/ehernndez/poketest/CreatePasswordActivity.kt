58025e2d402c6401ccafa19aa7eddef89f2f6a3c
Edwin <hgededwindaniel@gmail.com>
2024-04-15 20:21:05 -0600

    adding validations for uppercase and lowercase

--- app/src/main/java/com/ehernndez/poketest/CreatePasswordActivity.kt
+++ app/src/main/java/com/ehernndez/poketest/CreatePasswordActivity.kt
@@ -5,22 +5,78 @@ import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
 import android.widget.ImageView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import androidx.core.widget.addTextChangedListener
 import com.ehernndez.poketest.utils.Utils
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 import java.util.Locale
-import java.util.regex.Matcher
-import java.util.regex.Pattern
 
 class CreatePasswordActivity : AppCompatActivity() {
+    lateinit var imgEightCharacters: ImageView
+    lateinit var imgUpperCase: ImageView
+    lateinit var imgLowerCase: ImageView
+    lateinit var imgSpecialCharacter: ImageView
+    lateinit var imgANumber: ImageView
+    lateinit var containerPassword: TextInputLayout
+    lateinit var edtxtPassword: TextInputEditText
+    lateinit var containerConfirmPassword: TextInputLayout
+    lateinit var edtxtConfirmPassword: TextInputEditText
+    lateinit var btnNext: Button
+
+    val hasEightCharacters = 8
+    var hasUppercase = false
+    var hasLowercase = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_password)
 
+        imgEightCharacters = findViewById(R.id.img_eight_characters)
+        imgUpperCase = findViewById(R.id.img_uppercase)
+        imgLowerCase = findViewById(R.id.img_lowercase)
+        imgSpecialCharacter = findViewById(R.id.img_special_character)
+        imgANumber = findViewById(R.id.img_number)
+        containerPassword = findViewById(R.id.container_edtxt_password)
+        edtxtPassword = findViewById(R.id.edtxt_password)
+        containerConfirmPassword = findViewById(R.id.container_edtxt_confirm_password)
+        edtxtConfirmPassword = findViewById(R.id.edtxt_confirm_password)
+        btnNext = findViewById(R.id.btn_next)
+
+        edtxtPassword.addTextChangedListener(passwordTextWatcher)
+
+    }
+
+    private val passwordTextWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            hasUppercase = s.toString() != s.toString().lowercase(Locale.getDefault())
+            hasLowercase = s.toString() != s.toString().uppercase(Locale.getDefault())
+
+            if (hasUppercase) {
+                imgUpperCase.setImageResource(R.drawable.ic_checked)
+            } else {
+                imgUpperCase.setImageResource(R.drawable.ic_wrong)
+                Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+            }
+
+            if (hasLowercase) {
+                imgLowerCase.setImageResource(R.drawable.ic_checked)
+            } else {
+                imgLowerCase.setImageResource(R.drawable.ic_wrong)
+                Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+            }
+
+            if (s.toString().length == hasEightCharacters) {
+                imgEightCharacters.setImageResource(R.drawable.ic_checked)
+            } else {
+                imgEightCharacters.setImageResource(R.drawable.ic_wrong)
+                Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+            }
+        }
     }
 }
