8744c4c01ab6b6add7851277aeffdc7aee72c476
Edwin <hgededwindaniel@gmail.com>
2024-04-15 21:01:31 -0600

    adding validations to create new password

--- app/src/main/java/com/ehernndez/poketest/CreatePasswordActivity.kt
+++ app/src/main/java/com/ehernndez/poketest/CreatePasswordActivity.kt
@@ -1,6 +1,7 @@
 package com.ehernndez.poketest
 
 import android.os.Bundle
+import android.service.autofill.FieldClassification.Match
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.Button
@@ -9,7 +10,10 @@ import androidx.appcompat.app.AppCompatActivity
 import com.ehernndez.poketest.utils.Utils
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import org.w3c.dom.Text
 import java.util.Locale
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class CreatePasswordActivity : AppCompatActivity() {
     lateinit var imgEightCharacters: ImageView
@@ -26,6 +30,11 @@ class CreatePasswordActivity : AppCompatActivity() {
     val hasEightCharacters = 8
     var hasUppercase = false
     var hasLowercase = false
+
+    lateinit var patternDigits: Pattern
+    lateinit var hasADigit: Matcher
+    lateinit var patternSpecialCharacter: Pattern
+    lateinit var hasSpecialCharacter: Matcher
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -44,6 +53,33 @@ class CreatePasswordActivity : AppCompatActivity() {
 
         edtxtPassword.addTextChangedListener(passwordTextWatcher)
 
+        edtxtPassword.setOnFocusChangeListener { v, hasFocus ->
+            if (v.hasFocus()) {
+                if (edtxtPassword.text!!.toString() == edtxtConfirmPassword.text!!.toString()) {
+                    Utils().enableButton(this@CreatePasswordActivity, btnNext, true)
+                } else {
+                    Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+                }
+            }
+        }
+
+        edtxtConfirmPassword.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                if (edtxtPassword.text.toString() == s.toString()) {
+                    containerConfirmPassword.error = null
+                    Utils().enableButton(this@CreatePasswordActivity, btnNext, true)
+                } else {
+                    containerConfirmPassword.error = "La contraseña no coincide."
+                    Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+                }
+            }
+        })
     }
 
     private val passwordTextWatcher = object : TextWatcher {
@@ -53,9 +89,15 @@ class CreatePasswordActivity : AppCompatActivity() {
         override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
         }
 
-        override fun afterTextChanged(s: Editable?) {
-            hasUppercase = s.toString() != s.toString().lowercase(Locale.getDefault())
-            hasLowercase = s.toString() != s.toString().uppercase(Locale.getDefault())
+        override fun afterTextChanged(letter: Editable?) {
+            hasUppercase = letter.toString() != letter.toString().lowercase(Locale.getDefault())
+            hasLowercase = letter.toString() != letter.toString().uppercase(Locale.getDefault())
+
+            patternDigits = Pattern.compile("[0-9]")
+            hasADigit = patternDigits.matcher(letter.toString())
+
+            patternSpecialCharacter = Pattern.compile("[$&+,:;=\\\\?@#|/'<>.^*()%!-]")
+            hasSpecialCharacter = patternSpecialCharacter.matcher(letter.toString())
 
             if (hasUppercase) {
                 imgUpperCase.setImageResource(R.drawable.ic_checked)
@@ -71,11 +113,31 @@ class CreatePasswordActivity : AppCompatActivity() {
                 Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
             }
 
-            if (s.toString().length == hasEightCharacters) {
+            if (letter.toString().length == hasEightCharacters || edtxtConfirmPassword.text!!.length == hasEightCharacters) {
                 imgEightCharacters.setImageResource(R.drawable.ic_checked)
             } else {
                 imgEightCharacters.setImageResource(R.drawable.ic_wrong)
                 Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+            }
+
+            if (hasADigit.find()) {
+                imgANumber.setImageResource(R.drawable.ic_checked)
+            } else {
+                imgANumber.setImageResource(R.drawable.ic_wrong)
+                Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+            }
+
+            if (hasSpecialCharacter.find()) {
+                imgSpecialCharacter.setImageResource(R.drawable.ic_checked)
+            } else {
+                imgSpecialCharacter.setImageResource(R.drawable.ic_wrong)
+                Utils().enableButton(this@CreatePasswordActivity, btnNext, false)
+            }
+
+            if (edtxtPassword.text!!.isEmpty()) {
+                containerPassword.error = "Es necesario escribir la contraseña."
+            } else {
+                containerPassword.error = null
             }
         }
     }
