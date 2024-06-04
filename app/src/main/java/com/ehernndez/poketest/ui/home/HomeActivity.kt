796a87cdc4a87b8d98a78e27998d2c42c71d258b
Edwin <hgededwindaniel@gmail.com>
2024-06-03 21:00:28 -0600

    adding drawer menu

--- app/src/main/java/com/ehernndez/poketest/ui/home/HomeActivity.kt
+++ app/src/main/java/com/ehernndez/poketest/ui/home/HomeActivity.kt
@@ -1,40 +1,96 @@
 package com.ehernndez.poketest.ui.home
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.MenuItem
+import android.widget.TextView
 import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import com.ehernndez.poketest.R
+import com.ehernndez.poketest.data.persistantData.Data
 import com.ehernndez.poketest.ui.home.fragments.HomeFragment
 import com.ehernndez.poketest.ui.home.fragments.PokedexFragment
 import com.ehernndez.poketest.ui.home.fragments.PokemonFragment
-import com.ehernndez.poketest.utils.Utils
 import com.google.android.material.appbar.MaterialToolbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import com.google.android.material.navigation.NavigationView
 
 class HomeActivity : AppCompatActivity() {
     lateinit var navigationView: BottomNavigationView
     lateinit var toolbar: MaterialToolbar
+
+    lateinit var actionDrawerToggle: ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var drawerNavigation: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         navigationView = findViewById(R.id.navigation_view)
         toolbar = findViewById(R.id.home_toolbar)
+        drawerLayout = findViewById(R.id.drawer_layout_home)
+        drawerNavigation = findViewById(R.id.drawer_navigation_view)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         // setting first fragment to show when load the home activity
         replaceFragment(HomeFragment())
 
+        createBottomNavigationMenu()
+        createDrawerMenu()
+    }
+
+    private fun createDrawerMenu() {
+        actionDrawerToggle = ActionBarDrawerToggle(this, drawerLayout, R.string.txt_open_drawer, R.string.txt_close_drawer)
+        drawerLayout.addDrawerListener(actionDrawerToggle)
+        actionDrawerToggle.syncState()
+
+        drawerNavigation.setNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_notifications -> {
+                    Toast.makeText(this, "Notifications", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.item_share -> {
+                    Toast.makeText(this, "Share", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                R.id.item_info -> {
+                    Toast.makeText(this, "Info", Toast.LENGTH_SHORT).show()
+                    true
+                }
+
+                else -> false
+            }
+        }
+
+        val headerView = drawerNavigation.getHeaderView(0)
+        val txtUserName = headerView.findViewById<TextView>(R.id.txt_username)
+        val username = Data.shared.userName + " " + Data.shared.lastName
+        txtUserName.text = username
+
+        val txtBirthday = headerView.findViewById<TextView>(R.id.txt_birthday)
+        txtBirthday.text = Data.shared.bornDate
+
+        val txtEmail = headerView.findViewById<TextView>(R.id.txt_email)
+        txtEmail.text = Data.shared.email
+
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (actionDrawerToggle.onOptionsItemSelected(item)) {
+            return false
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun createBottomNavigationMenu() {
         navigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.item_home -> {
@@ -55,82 +111,6 @@ class HomeActivity : AppCompatActivity() {
                 else -> false
             }
         }
-
-        toolbar.setOnMenuItemClickListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.item_settings -> {
-                    Utils().createIntent(this@HomeActivity, SettingsActivity())
-
-                    // Calling launch coroutine function
-                    // helloWorldCoroutine()
-
-                    // Calling async coroutine function
-                    //helloWorldCoroutineAsync()
-
-                    // Calling differents types of dispatchers
-                    // dispatchers()
-
-                    // calling suspend fun function
-                    // callingDoSomething()
-                    true
-                }
-
-                else -> false
-            }
-        }
-    }
-
-    fun helloWorldCoroutine() {
-        GlobalScope.launch {
-            delay(10000L)
-            Log.e("[Coroutines] --->", "Hello world")
-
-        }
-        Log.e("[Thread] --->", "Hello world from thread")
-        Thread.sleep(5000L)
-    }
-
-    fun helloWorldCoroutineAsync() {
-        GlobalScope.launch {
-            val result = async {
-                calculateResult()
-            }
-            Log.e("[Coroutines] --->", "calculated result: ${result.await()}")
-        }
-        Thread.sleep(2000L)
-    }
-
-    suspend fun calculateResult(): Int {
-        delay(3000L)
-        return 100
-    }
-
-    fun dispatchers() = runBlocking {
-        launch(Dispatchers.IO) {
-            Log.e("[Coroutines] --->", "Disptachers.IO --> ${Thread.currentThread().name}")
-        }
-
-        launch(Dispatchers.Default) {
-            Log.e("[Coroutines] --->", "Dispatches.Default --> ${Thread.currentThread().name}")
-        }
-
-        launch(Dispatchers.Main) {
-            Log.e(
-                "[Coroutines] --->",
-                "Dispatchers.Main --> ${Thread.currentThread().name}"
-            )
-        }
-    }
-
-    suspend fun doSomething() {
-        delay(5000L)
-        Log.e("[Coroutines] --->", "Calling doSomething function")
-    }
-
-    fun callingDoSomething() = runBlocking {
-        launch {
-            doSomething()
-        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
@@ -139,3 +119,16 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 }
+
+
+// this code is used to open SettingsActivity
+/* toolbar.setOnMenuItemClickListener { menuItem ->
+     when (menuItem.itemId) {
+         R.id.item_settings -> {
+             Utils().createIntent(this@HomeActivity, SettingsActivity())
+             true
+         }
+
+         else -> false
+     }
+ } */
